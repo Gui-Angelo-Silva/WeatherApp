43d5d0c90316817bd18a2aa3fc8f60a1c63188db
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2024-12-21 11:47:08 -0300

    :sparkles: feat: Adicionando Icons.

--- WeatherApp/WeatherForm.Designer.cs
+++ WeatherApp/WeatherForm.Designer.cs
@@ -32,94 +32,113 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+			DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WeatherForm));
 			lblCity = new Label();
 			lblDescription = new Label();
 			dgvForecast = new DataGridView();
 			lblTemperature = new Label();
 			panelForecast = new Panel();
+			btnOtherView = new Button();
 			((System.ComponentModel.ISupportInitialize)dgvForecast).BeginInit();
+			panelForecast.SuspendLayout();
 			SuspendLayout();
-
 			// 
 			// lblCity
 			// 
 			lblCity.AutoSize = true;
-			lblCity.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			lblCity.Font = new Font("Segoe UI", 12F);
+			lblCity.ForeColor = Color.FromArgb(0, 123, 255);
 			lblCity.Location = new Point(12, 9);
 			lblCity.Name = "lblCity";
-			lblCity.Size = new Size(100, 21);
+			lblCity.Size = new Size(83, 21);
 			lblCity.TabIndex = 0;
 			lblCity.Text = "Cidade: ---";
-			lblCity.ForeColor = Color.FromArgb(0, 123, 255);
-
 			// 
 			// lblDescription
 			// 
 			lblDescription.AutoSize = true;
-			lblDescription.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			lblDescription.Font = new Font("Segoe UI", 12F);
+			lblDescription.ForeColor = Color.FromArgb(40, 167, 69);
 			lblDescription.Location = new Point(12, 40);
 			lblDescription.Name = "lblDescription";
-			lblDescription.Size = new Size(130, 21);
+			lblDescription.Size = new Size(115, 21);
 			lblDescription.TabIndex = 1;
 			lblDescription.Text = "Clima Atual: ---";
-			lblDescription.ForeColor = Color.FromArgb(40, 167, 69);
-
 			// 
 			// dgvForecast
 			// 
+			dataGridViewCellStyle1.BackColor = Color.LightBlue;
+			dgvForecast.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
 			dgvForecast.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+			dgvForecast.BackgroundColor = Color.White;
+			dgvForecast.BorderStyle = BorderStyle.None;
+			dgvForecast.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
 			dgvForecast.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle2.BackColor = SystemColors.Window;
+			dataGridViewCellStyle2.Font = new Font("Segoe UI", 10F);
+			dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
+			dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+			dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+			dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+			dgvForecast.DefaultCellStyle = dataGridViewCellStyle2;
+			dgvForecast.GridColor = Color.LightGray;
 			dgvForecast.Location = new Point(0, 0);
 			dgvForecast.Name = "dgvForecast";
 			dgvForecast.RowHeadersWidth = 51;
 			dgvForecast.RowTemplate.Height = 29;
 			dgvForecast.Size = new Size(784, 358);
 			dgvForecast.TabIndex = 2;
-			dgvForecast.DefaultCellStyle.Font = new Font("Segoe UI", 10F);
-			dgvForecast.BackgroundColor = Color.White;
-			dgvForecast.BorderStyle = BorderStyle.None;
-			dgvForecast.GridColor = Color.LightGray;
-			dgvForecast.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
-			dgvForecast.AlternatingRowsDefaultCellStyle.BackColor = Color.LightBlue;
-
-			// 
-			// panelForecast
-			// 
-			panelForecast.AutoScroll = true;
-			panelForecast.Location = new Point(12, 80);
-			panelForecast.Name = "panelForecast";
-			panelForecast.Size = new Size(784, 358);
-			panelForecast.TabIndex = 4;
-			panelForecast.Controls.Add(dgvForecast);
-
 			// 
 			// lblTemperature
 			// 
 			lblTemperature.AutoSize = true;
-			lblTemperature.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			lblTemperature.Font = new Font("Segoe UI", 12F);
+			lblTemperature.ForeColor = Color.FromArgb(255, 87, 34);
 			lblTemperature.Location = new Point(196, 9);
 			lblTemperature.Name = "lblTemperature";
-			lblTemperature.Size = new Size(125, 21);
+			lblTemperature.Size = new Size(122, 21);
 			lblTemperature.TabIndex = 3;
 			lblTemperature.Text = "Temperatura: ---";
-			lblTemperature.ForeColor = Color.FromArgb(255, 87, 34);
-
+			// 
+			// panelForecast
+			// 
+			panelForecast.AutoScroll = true;
+			panelForecast.Controls.Add(dgvForecast);
+			panelForecast.Location = new Point(12, 80);
+			panelForecast.Name = "panelForecast";
+			panelForecast.Size = new Size(784, 358);
+			panelForecast.TabIndex = 4;
+			// 
+			// btnOtherView
+			// 
+			btnOtherView.Cursor = Cursors.Hand;
+			btnOtherView.Location = new Point(701, 12);
+			btnOtherView.Name = "btnOtherView";
+			btnOtherView.Size = new Size(95, 27);
+			btnOtherView.TabIndex = 5;
+			btnOtherView.Text = "Aprimorado";
+			btnOtherView.UseVisualStyleBackColor = true;
 			// 
 			// WeatherForm
 			// 
+			BackColor = Color.FromArgb(248, 249, 250);
 			ClientSize = new Size(808, 461);
+			Controls.Add(btnOtherView);
 			Controls.Add(lblTemperature);
 			Controls.Add(panelForecast);
 			Controls.Add(lblDescription);
 			Controls.Add(lblCity);
-			Name = "WeatherForm";
-			Text = "Previsão do Tempo";
-			BackColor = Color.FromArgb(248, 249, 250);
 			FormBorderStyle = FormBorderStyle.FixedDialog;
+			Icon = (Icon)resources.GetObject("$this.Icon");
+			Name = "WeatherForm";
 			StartPosition = FormStartPosition.CenterScreen;
+			Text = "Previsão do Tempo";
 			Load += WeatherForm_Load;
-
 			((System.ComponentModel.ISupportInitialize)dgvForecast).EndInit();
+			panelForecast.ResumeLayout(false);
 			ResumeLayout(false);
 			PerformLayout();
 		}
@@ -128,5 +147,6 @@
 
 		private Label lblTemperature;
 		private Panel panelForecast;
+		private Button btnOtherView;
 	}
 }
